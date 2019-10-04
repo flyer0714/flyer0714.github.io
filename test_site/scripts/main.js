64adc6932df0f15219ad5c9e3152e0d5491c245d
flyer0714 <42204472+flyer0714@users.noreply.github.com>
2019-10-04 19:50:05 +0800

    fail with extra comma

--- test_site/scripts/main.js
+++ test_site/scripts/main.js
@@ -15,7 +15,7 @@ var myHeading = document.querySelector('h1');
 function setUsername(){
     var myName = prompt('Please enter your name.');
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ', + myName;
+    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
 }
 
 if(!localStorage.getItem('name')){
